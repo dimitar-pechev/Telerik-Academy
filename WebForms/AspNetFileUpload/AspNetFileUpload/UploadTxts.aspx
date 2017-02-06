bfb9063844a1aa02472098e1823d5c64082df9dd
dimitar-pechev <dimpechev@gmail.com>
2017-02-06 18:44:35 +0200

    file upload homework

---
+++ WebForms/AspNetFileUpload/AspNetFileUpload/UploadTxts.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Upload Stuff" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="UploadTxts.aspx.cs" Inherits="AspNetFileUpload.UploadTxts" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ccl" %>
+
+<asp:Content ID="UploadStuff" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:ScriptManager runat="server" />
+    <div class="row">
+        <ccl:AjaxFileUpload runat="server"
+            AllowedFileTypes="zip"
+            MaximumNumberOfFiles="1"
+            MaxFileSize="102400"
+            ID="UploadForm"
+            OnUploadComplete="UploadForm_UploadComplete" />
+        <br />
+        <p class="text-center">Only .zip files allowed! If the .zip contains files different from plain text format, they won't be read and stored to the DB properly!</p>
+        <br />
+        <asp:Button Text="Get Files Text from DB" CssClass="btn btn-success" runat="server" ID="BtnGetFilesFromDb" OnClick="BtnGetFilesFromDb_Click" />
+        <asp:Panel runat="server" ID="Result" Visible="false">
+            <h3 class="text-center">Extracted text</h3>
+        </asp:Panel>
+    </div>
+</asp:Content>
---
+++ WebForms/AspNetFileUpload/AspNetFileUpload/UploadTxts.aspx.cs
@@ -0,0 +1,65 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspNetFileUpload
+{
+    public partial class UploadTxts : System.Web.UI.Page
+    {
+        private TxtFilesEntities db = new TxtFilesEntities();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadForm_UploadComplete(object sender, AjaxControlToolkit.AjaxFileUploadEventArgs e)
+        {
+            var result = new Dictionary<string, string>();
+            using (ZipFile zip = ZipFile.Read(e.GetStreamContents()))
+            {
+                foreach (ZipEntry entry in zip)
+                {
+                    MemoryStream data = new MemoryStream();
+                    entry.Extract(data);
+                    var resString = Encoding.UTF8.GetString(data.ToArray());
+                    result.Add(entry.FileName, resString);
+                }
+            }
+
+            foreach (var file in result)
+            {
+                var fileArr = file.Key.Split('.').ToList();
+                var fileExtesion = fileArr.Last();
+                fileArr.RemoveAt(fileArr.Count - 1);
+                var fileName = string.Join("", fileArr);
+                var txtFile = new AspNetFileUpload.File() { FileName = fileName, FileExtension = fileExtesion, FileContent = file.Value };
+
+                this.db.Files.Add(txtFile);
+            }
+
+            this.db.SaveChanges();
+        }
+
+        protected void BtnGetFilesFromDb_Click(object sender, EventArgs e)
+        {
+            var files = this.db.Files.ToList();
+            files.Reverse();
+
+            foreach (var file in files)
+            {
+                var label = new Label();
+                label.Text = $"<b>{file.FileName}.{file.FileExtension}</b><br /> {file.FileContent}<br />";
+                this.Result.Controls.Add(label);
+            }
+
+            this.Result.Visible = true;
+        }
+    }
+}
